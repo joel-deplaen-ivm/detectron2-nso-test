9da33b34a50fa42a7bd9c42b807c7bcbeabcc64d
joel-deplaen-ivm <107033846+joel-deplaen-ivm@users.noreply.github.com>
2023-05-12 16:02:46 +0200

    comments in script

--- 1_data_preperation/prepare_imagery/download_NSO.sh
+++ 1_data_preperation/prepare_imagery/download_NSO.sh
@@ -1,40 +1,44 @@
 #!/bin/bash
 
+### This script reads a list of FTP URLs from a CSV file, downloads the corresponding files using curl, ###
+### unzips them, and moves the resulting TIFF files to a new directory. ###
+
+
 # Read the URLs from the CSV file
-urls=()
-while IFS=, read -r url; do
-    urls+=("$url")
-done < ftp_urls_test.csv
+urls=()  # Initialize an empty array called 'urls'
+while IFS=, read -r url; do  # Loop through each line of the CSV file
+    urls+=("$url")  # Add the current URL to the 'urls' array
+done < ftp_urls_test.csv  # Read from the CSV file called 'ftp_urls_test.csv'
 
 # Make and move to the directory where the files will be downloaded
-mkdir -p NSO_raw
+mkdir -p NSO_raw  
 cd NSO_raw
 
-# Loop through the list of URLs and download each file
-for url in "${urls[@]}"
+# Loop through the list of URLs in array and download each file
+for url in "${urls[@]}" 
 do
     # Get the filename from the URL
-    filename=$(basename "$url")
+    filename=$(basename "$url")  
 
     # Download the file using curl
-    if curl -u 'username:password!' -O --ftp-pasv --ssl --insecure "$url"; then
-        echo "Downloaded $filename successfully"
+    if curl -u 'username:password' -O --ftp-pasv --ssl --insecure "$url"; then  
+        echo "Downloaded $filename successfully"  
     else
-        echo "Error downloading $filename"
+        echo "Error downloading $filename"  
     fi
 done
 
 # Unzip the files and move them to a new directory
-mkdir -p NSO_unzip
+mkdir -p NSO_unzip  
 
 # Loop through the list of zip files in the current directory and unzip them to the new directory
 for file in *.zip
 do
-    unzip "$file" -d NSO_unzip/
+    unzip "$file" -d NSO_unzip/ 
 done
 
 # Move .tif to NSO directory
-mkdir -p ../../NSO/ 
-mv NSO_unzip/*.tif ../../NSO/
+mkdir -p ../../NSO/  
+mv NSO_unzip/*.tif ../../NSO/  
 
 echo "Done downloading and organzing imagery dataset"
